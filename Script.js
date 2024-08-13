645b9dc7995dadf87756c9116c34c7c92237f9c3
Alfonso <diegoquerrales90@gmail.com>
2024-08-13 19:33:23 -0400

    header nav

---
+++ Script.js
@@ -0,0 +1,9 @@
+function showSidebar (){
+    const BarraLateral = document.querySelector('.BarraLateral')
+    BarraLateral.style.display = 'Flex'
+}
+
+function hideSidebar (){
+    const BarraLateral = document.querySelector('.BarraLateral')
+    BarraLateral.style.display = 'none'
+}
